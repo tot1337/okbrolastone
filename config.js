1b48fd36eb660fb1870c0127b16e3e2116d3d85d
tot <htagattalt@gmail.com>
2025-08-30 23:03:38 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
-  profilePicture: "https://cdn.discordapp.com/avatars/1390015184323612692/b51d6a1f1a4dfdf235ff6740eb497358.png",
-  popupPicture: "https://cdn.discordapp.com/avatars/1390015184323612692/b51d6a1f1a4dfdf235ff6740eb497358.png",
+  profilePicture: "https://media.discordapp.net/attachments/1056288920184307812/1411440208368504994/tot.jpg?ex=68b4a9a0&is=68b35820&hm=b4fd97d99f00138caba4f682e4e62cc5000dce96fbedbedf4bcadcde7434137e&=&format=webp&width=931&height=931",
+  popupPicture: "https://media.discordapp.net/attachments/1056288920184307812/1411440208368504994/tot.jpg?ex=68b4a9a0&is=68b35820&hm=b4fd97d99f00138caba4f682e4e62cc5000dce96fbedbedf4bcadcde7434137e&=&format=webp&width=931&height=931",
   popupQuote: "But... the sensation that I've lost something lingers for a longtime after I wake up.",
   userName: "@zeytin",
   links: [
