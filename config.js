b5e427b2312ffefa1f6ae10f91d5f2883c92970e
tot <htagattalt@gmail.com>
2025-08-30 04:25:40 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const config = {
+  profilePicture: "https://cdn.discordapp.com/avatars/1390015184323612692/b51d6a1f1a4dfdf235ff6740eb497358.png",
+  popupPicture: "https://cdn.discordapp.com/avatars/1390015184323612692/b51d6a1f1a4dfdf235ff6740eb497358.png",
+  popupQuote: "But... the sensation that I've lost something lingers for a longtime after I wake up.",
+  userName: "@zeytin",
+  links: [
+    { href: "https://github.com/tot1337", icon: "fab fa-github", label: "Github" },
+    { href: "https://www.instagram.com/192.168.3.6", icon: "fab fa-instagram", label: "Instagram" }
+  ]
+};
